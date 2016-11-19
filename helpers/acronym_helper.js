8a79e955ae1b9cf362e3130cadf82e0a5af7fd4e
Alex Emrie <emrieaj@gmail.com>
2016-11-19 12:17:43 -0800

    Refactor acronym helper

--- helpers/acronym_helper.js
+++ helpers/acronym_helper.js
@@ -2,33 +2,16 @@ var acronymHelper = function(){
     var that = this;
     return {
         findAcronyms: function(string){
-            var periodArray = string.split(".");
-
+            var spaceArray = string.split(" ");
             var result = [];
-            var part = [];
 
-            for(var i = 0; i < periodArray.length; i++) {
-                var currElem = periodArray[i];
-                var currElemLength = currElem.length;
+            for (var i = 0; i < spaceArray.length; i++) {
+                var currElem = spaceArray[i];
 
-                if (currElemLength == 1 && periodArray[i + 1].length == 1) {
-                    if (part.length < 1) {
-                        var lastElemPrev = periodArray[i - 1].slice(-1);
-                        part.push(lastElemPrev);
-                    }
-
-                    part.push(currElem);
+                if (currElem.match(/[.][A-Z][.]/)) {
+                    result.push(currElem);
                 }
-
-                if ((i > 1 && periodArray[i - 1].length == 1 && currElemLength > 1) || (currElem == "" && periodArray[i - 1].length == 1)) {
-                    var lastElemPrev = periodArray[i - 1].slice(-1);
-                    part.push(lastElemPrev);
-                    var acronym = part.join('.') + ".";
-
-                    result.push(acronym);
-                    part = [];
-                }
-            };
+            }
 
             return result;
         }
